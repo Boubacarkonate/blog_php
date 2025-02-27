1ce75d71533ad8a9dff6e36315abeb250d5887b6
Bouba <boubacar.konate@outlook.fr>
2025-02-27 18:12:06 +0100

    features : pages main + style ok

--- index.php
+++ index.php
@@ -1,7 +1,251 @@
-<?php require_once 'includes/header.php'; ?>
+<?php require 'includes/header.php'; ?>
+<div class="container">
+    <main class="my-5">
+        <div class="title p-5 rounded-pill w-50 mx-auto">
+            <h1 class="text-center">MediCare Blog</h1>
+            <p class="text-center">Explorez l’Univers du Médico-Social</p>
+        </div>
+        <section class="d-grid gap-5 pt-5 ">
+            <article class="text-center">
+                <h2 class="pb-2 hover-shadow">Le Secteur Médico-Social en France : Un Pilier Essentiel de
+                    l'Accompagnement et
+                    du
+                    Soin
+                </h2>
+                <p class="fw-semibold fs-5">Le secteur médico-social en France occupe une place essentielle dans la
+                    prise
+                    en
+                    charge des
+                    populations
+                    vulnérables. Il regroupe un ensemble d'établissements, de services et de professionnels dédiés à
+                    l'accompagnement des personnes en situation de handicap, des personnes âgées dépendantes, ainsi que
+                    des
+                    populations précaires ou fragilisées.
+                </p>
+            </article>
+        </section>
 
-<main>
-    <!-- contenu du blog -->
-</main>
+        <section class="d-grid gap-5 pt-5 ">
+            <article>
+                <div class="row gx-0 ">
+                    <!-- Carte 1 -->
+                    <div class="col-12 col-md-6">
+                        <div class="card h-100 border border-0">
+                            <div class="card-body d-flex flex-column">
+                                <h2 class="card-title pb-4">À propos de <span>MediCare Blog</span></h2>
+                                <p class="card-text">
+                                    Bienvenue sur [Nom du Blog], un espace dédié à l'univers du
+                                    médico-social en France. Nous avons créé
+                                    ce blog pour informer, sensibiliser et accompagner toutes les personnes concernées
+                                    par ce secteur
+                                    essentiel : professionnels, aidants, patients et familles.
+                                    <br>
+                                    Derrière ce blog, une équipe passionnée par le secteur médico-social, composée de
+                                    professionnels de
+                                    la santé, de travailleurs sociaux et de citoyens engagés. Notre objectif est simple
+                                    : offrir une
+                                    plateforme d'information et d'échange sur le médico-social en France.
+                                    <br>
+                                    Nous proposons des analyses sur les réformes et évolutions du secteur, des conseils
+                                    pratiques pour
+                                    les aidants et les familles, des témoignages inspirants de professionnels et de
+                                    bénéficiaires, ainsi
+                                    que des ressources utiles pour mieux accompagner et soutenir les personnes
+                                    concernées.
+                                    <br>
+                                    Nous croyons fermement à l’accessibilité, en rendant l’information claire et
+                                    compréhensible pour
+                                    tous, à la bienveillance, en abordant chaque sujet avec humanité et empathie, et à
+                                    l’engagement, en
+                                    mettant en avant les acteurs du secteur et les défis qu'ils relèvent au quotidien.
+                                    <br>
+                                    Merci de faire partie de cette communauté engagée pour un médico-social plus humain
+                                    et accessible à
+                                    tous !
+                                </p>
+                            </div>
+                        </div>
+                    </div>
 
-<?php require_once 'includes/footer.php'; ?>
+                    <!-- Image à côté de la carte -->
+                    <div class="col-12 col-md-6">
+                        <div class="card d-flex justify-content-center align-items-center h-100 border border-0">
+                            <div class="card-body text-center w-75">
+                                <img src="./public/solidarite.png" alt="Logo" class="img-fluid h-100">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </article>
+        </section>
+        <section class="d-grid gap-5 pt-5">
+            <article>
+                <h2 class="p-2">Qu'est-ce que le secteur médico-social ?</h2>
+                <p>Le secteur médico-social est une interface entre le domaine médical et le secteur
+                    social. Il vise à
+                    assurer une prise en charge globale des personnes ayant besoin d'un accompagnement spécifique, tant
+                    sur
+                    le plan de la santé que de l'insertion sociale.</p>
+                <ul>
+                    <h3>Les principales missions du secteur médico-social :</h3>
+                    <li>Accompagner les personnes en situation de handicap dans leur quotidien et leur parcours
+                        d'insertion
+                        sociale et professionnelle.</li>
+                    <li>Prendre en charge les personnes âgées en perte d'autonomie, notamment via les EHPAD et les
+                        services
+                        d'aide à domicile.</li>
+                    <li>Soutenir les personnes en grande précarité par le biais de structures d'accueil et d'insertion.
+                    </li>
+                    <li>Proposer un suivi et une assistance aux personnes atteintes de maladies chroniques ou de
+                        troubles
+                        psychiques.</li>
+                </ul>
+            </article>
+            <article>
+                <h2 class="p-2">Les acteurs du secteur médico-social</h2>
+                <ul>
+                    <p>
+                        Le secteur médico-social repose sur une multitude d'acteurs publics et privés qui travaillent de
+                        concert pour assurer une prise en charge efficiente des populations concernées. Parmi eux, on
+                        retrouve :
+                    </p>
+                    <li><span class="acteursDuSecteur">Les établissements et services sociaux et médico-sociaux (ESSMS)
+                            :</span> EHPAD, maisons
+                        d'accueil spécialisées, foyers d'accueil médicalisés, instituts médico-éducatifs (IME), services
+                        d'aide à domicile (SAAD), etc.</li>
+                    <li>
+
+                        <span class="acteursDuSecteur">Les professionnels de santé et du social :</span>
+                        aides-soignants,
+                        infirmiers, travailleurs
+                        sociaux,
+                        psychologues, ergothérapeutes, médecins coordonnateurs, etc.
+                    </li>
+                    <li><span class="acteursDuSecteur">Les collectivités territoriales et l'État :</span> conseils
+                        départementaux, Agences Régionales
+                        de Santé (ARS), Caisse Nationale de Solidarite pour l'Autonomie (CNSA).</li>
+                    <li><span class="acteursDuSecteur">Les associations et organismes privés :</span> de nombreuses
+                        associations jouent un rôle crucial dans la gestion et l'animation des structures
+                        médico-sociales.
+                    </li>
+                </ul>
+            </article>
+            <article>
+                <h2 class="p-2">Les enjeux et défis du secteur</h2>
+                <p>Le secteur médico-social est confronté à de nombreux défis qui nécessitent des adaptations constantes
+                    pour répondre aux besoins croissants de la population.</p>
+
+
+                <div class="row gx-0 pt-5">
+                    <!-- Carte 1 -->
+                    <div class="col-12 col-md-6">
+                        <div class="card h-100 border border-0">
+                            <div
+                                class="card-body d-flex justify-content-center align-items-center flex-column text-center">
+                                <h3 class="card-title pb-4">Vieillissement de la population et prise en charge des
+                                    personnes âgées</h3>
+                                <p class="card-text">La France fait face à un vieillissement démographique
+                                    important, ce qui engendre une augmentation des besoins en structures d'accueil
+                                    et en soins à domicile pour les personnes âgées.</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Image à côté de la carte -->
+                    <div class="col-12 col-md-6">
+                        <div class="card d-flex justify-content-center align-items-center h-100 border border-0">
+                            <div class="card-body text-center w-75">
+                                <img src="./public/pers.jpg" alt="Logo" class="img-fluid h-100">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row gx-0 pt-5">
+                    <!-- Image à côté de la carte -->
+                    <div class="col-12 col-md-6">
+                        <div class="card d-flex justify-content-center align-items-center h-100 border border-0">
+                            <div class="card-body text-center w-75">
+                                <img src="./public/outil.jpg" alt="Logo" class="img-fluid h-100">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-6">
+                        <div class="card h-100 border border-0">
+                            <div
+                                class="card-body d-flex justify-content-center align-items-center flex-column text-center ">
+                                <h3 class="card-title pb-4">Inclusion des personnes en situation de handicap</h3>
+                                <p class="card-text">Les politiques publiques tendent à favoriser l'inclusion des
+                                    personnes en situation de handicap dans la société, notamment en développant des
+                                    solutions alternatives à l'institutionnalisation et en promouvant
+                                    l'accessibilité.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- Deuxième ligne de cartes -->
+                <div class="row gx-0 pt-5">
+                    <div class="col-12 col-md-6">
+                        <div class="card p-3 h-100  border border-0">
+                            <div
+                                class="card-body d-flex justify-content-center align-items-center flex-column text-center ">
+                                <h3 class="card-title pb-4">Manque de personnel et conditions de travail</h3>
+                                <p class="card-text">Le secteur médico-social souffre d'une pénurie de
+                                    professionnels, notamment en raison de conditions de travail exigeantes et d'une
+                                    reconnaissance encore insuffisante. L'amélioration des salaires et des
+                                    conditions de travail est un enjeu majeur.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-6">
+                        <div class="card d-flex justify-content-center align-items-center h-100 border border-0">
+                            <div class="card-body text-center w-75">
+                                <img src="./public/personnes.png" alt="Logo" class="img-fluid h-100">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row gx-0 pt-5">
+                    <div class="col-12 col-md-6">
+                        <div class="card d-flex justify-content-center align-items-center h-100 border border-0 ">
+                            <div class="card-body text-center w-75">
+                                <img src="./public/informatique.jpg" alt="Logo" class="img-fluid h-100">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-6">
+                        <div class="card h-100 border border-0">
+                            <div
+                                class="card-body d-flex justify-content-center align-items-center flex-column text-center">
+                                <h3 class="card-title pb-4">Numérisation et modernisation des services</h3>
+                                <p class="card-text">L'essor des technologies numériques offre de nouvelles perspectives
+                                    pour améliorer la qualité des services (télémédecine, dossiers médicaux partagés,
+                                    outils d'aide à la décision pour les professionnels).</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+            </article>
+
+            <article>
+                <h2 class="p-2">Perspectives d'avenir</h2>
+                <p>L'avenir du secteur médico-social repose sur une meilleure coordination entre les différents acteurs,
+                    une
+                    adaptation des infrastructures aux besoins émergents, ainsi qu'une reconnaissance accrue des
+                    professionnels du secteur.
+                    <br>
+                    Les politiques publiques devront continuer à s'orienter vers une approche plus inclusive et
+                    personnalisée, permettant à chacun de bénéficier d'un accompagnement adapté à sa situation.
+                    <br>
+                    En somme, le secteur médico-social est un pilier essentiel du système de santé et de solidarité en
+                    France. Il est appelé à évoluer pour relever les défis actuels et futurs, avec pour objectif
+                    principal
+                    le bien-être et la dignité des personnes accompagnées.
+                </p>
+            </article>
+        </section>
+    </main>
+</div>
+</div>
+<?php require 'includes/footer.php'; ?>
